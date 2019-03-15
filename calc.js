7f3e3c42c6277f7fab655ec19d73bdeb2f439b5c
Bobby <bobbysebolao@gmail.com>
2019-03-15 00:05:28 +0000

    One test added for each mathematical operation

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
-// let calculator = document.querySelector('.calculator');
-// let display = document.querySelector('.display');
-// let keys = calculator.querySelector('.keys');
+let calculator = document.querySelector('.calculator');
+let display = document.querySelector('.display');
+let keys = calculator.querySelector('.keys');
 
 let calculate = (n1, operator, n2) => {
   let result = '';
@@ -19,80 +19,78 @@ let calculate = (n1, operator, n2) => {
   return result;
 }
 
-// let final = keys.addEventListener("click", click => {
-//   if (click.target.matches('button')) {
-//     let key = click.target;
-//     let action = key.dataset.action;
-//     let keyContent = key.textContent;
-//     let displayedNum = display.textContent;
-//     let previousKeyType = calculator.dataset.previousKeyType;
-//
-//     if (!action) {
-//       if (displayedNum == '0' || previousKeyType == 'operator') {
-//         display.textContent = keyContent;
-//
-//       }
-//       if (displayedNum != '0' && previousKeyType != 'operator') {
-//         display.textContent = display.textContent.concat(keyContent);
-//       }
-//
-//     }
-//
-//     if (action == 'add') {
-//         console.log('add key!');
-//         calculator.dataset.previousKeyType = 'operator';
-//         calculator.dataset.firstNum = displayedNum;
-//         calculator.dataset.operator = action;
-//       }
-//
-//     if (action == 'subtract') {
-//         console.log('subtract key!');
-//         calculator.dataset.previousKeyType = 'operator';
-//         calculator.dataset.firstNum = displayedNum;
-//         calculator.dataset.operator = action;
-//       }
-//     if (action == 'multiply') {
-//         console.log('multiply key!');
-//         calculator.dataset.previousKeyType = 'operator';
-//         calculator.dataset.firstNum = displayedNum;
-//         calculator.dataset.operator = action;
-//       }
-//     if (action == 'divide') {
-//         console.log('divide key!');
-//         calculator.dataset.previousKeyType = 'operator';
-//         calculator.dataset.firstNum = displayedNum;
-//         calculator.dataset.operator = action;
-//       }
-//     if (action == 'decimal' && display.textContent.includes('.') == false) {
-//       display.textContent = display.textContent.concat(keyContent);
-//       }
-//     if (action == 'clear') {
-//         console.log('clear key!');
-//         display.textContent = 0;
-//         calculator.dataset.previousKeyType = '';
-//         calculator.dataset.firstNum = '';
-//         calculator.dataset.operator = '';
-//         let firstNum = '';
-//         let secondNum = '';
-//
-//       }
-//     if (action == 'calculate') {
-//         console.log('calculate key!');
-//         let firstNum = calculator.dataset.firstNum;
-//         let operator = calculator.dataset.operator;
-//         let secondNum = displayedNum;
-//
-//         display.textContent = calculate(firstNum, operator, secondNum);
-//       }
-//
-//   }
-//
-// });
+let final = keys.addEventListener("click", click => {
+  if (click.target.matches('button')) {
+    let key = click.target;
+    let action = key.dataset.action;
+    let keyContent = key.textContent;
+    let displayedNum = display.textContent;
+    let previousKeyType = calculator.dataset.previousKeyType;
+
+    if (!action) {
+      if (displayedNum == '0' || previousKeyType == 'operator') {
+        display.textContent = keyContent;
+
+      }
+      if (displayedNum != '0' && previousKeyType != 'operator') {
+        display.textContent = display.textContent.concat(keyContent);
+      }
+
+    }
+
+    if (action == 'add') {
+        console.log('add key!');
+        calculator.dataset.previousKeyType = 'operator';
+        calculator.dataset.firstNum = displayedNum;
+        calculator.dataset.operator = action;
+      }
+
+    if (action == 'subtract') {
+        console.log('subtract key!');
+        calculator.dataset.previousKeyType = 'operator';
+        calculator.dataset.firstNum = displayedNum;
+        calculator.dataset.operator = action;
+      }
+    if (action == 'multiply') {
+        console.log('multiply key!');
+        calculator.dataset.previousKeyType = 'operator';
+        calculator.dataset.firstNum = displayedNum;
+        calculator.dataset.operator = action;
+      }
+    if (action == 'divide') {
+        console.log('divide key!');
+        calculator.dataset.previousKeyType = 'operator';
+        calculator.dataset.firstNum = displayedNum;
+        calculator.dataset.operator = action;
+      }
+    if (action == 'decimal' && display.textContent.includes('.') == false) {
+      display.textContent = display.textContent.concat(keyContent);
+      }
+    if (action == 'clear') {
+        console.log('clear key!');
+        display.textContent = 0;
+        calculator.dataset.previousKeyType = '';
+        calculator.dataset.firstNum = '';
+        calculator.dataset.operator = '';
+        let firstNum = '';
+        let secondNum = '';
+
+      }
+    if (action == 'calculate') {
+        console.log('calculate key!');
+        let firstNum = calculator.dataset.firstNum;
+        let operator = calculator.dataset.operator;
+        let secondNum = displayedNum;
+
+        display.textContent = calculate(firstNum, operator, secondNum);
+      }
+
+  }
+
+});
 
 module.exports = {
-    sayHello: function() {
-      return "Hello";
-    },
+    calculate: calculate,
   };
 
 // module.exports = calculate;
--- calc.test.js
+++ calc.test.js
@@ -7,7 +7,7 @@ const DOM = new JSDOM(html);
 
 global.document = DOM.window.document;
 
-let logic = require('./calc.js');
+let calculator = require('./calc.js');
 
 test("Tape is working", function(t) {
   t.equal(1, 1, "Input 1 should equal 1");
@@ -15,8 +15,29 @@ test("Tape is working", function(t) {
 });
 
 test("Adds two numbers", function(t) {
-  const actual = logic.sayHello;
-  const expected = 5;
-  t.equal(actual, expected, "2 + 2 = 5");
+  const actual = calculator.calculate('2', 'add', '2');
+  const expected = 4;
+  t.equal(actual, expected, "2 + 2 = 4");
+  t.end();
+});
+
+test("Subtracts two numbers", function(t) {
+  const actual = calculator.calculate('20', 'subtract', '2');
+  const expected = 18;
+  t.equal(actual, expected, "20 - 2 = 18");
+  t.end();
+});
+
+test("Multiplies two numbers", function(t) {
+  const actual = calculator.calculate('14', 'multiply', '4');
+  const expected = 56;
+  t.equal(actual, expected, "14 * 4 = 56");
+  t.end();
+});
+
+test("Divides two numbers", function(t) {
+  const actual = calculator.calculate('17', 'divide', '2');
+  const expected = 8.5;
+  t.equal(actual, expected, "17 / 2 = 8.5");
   t.end();
 });
